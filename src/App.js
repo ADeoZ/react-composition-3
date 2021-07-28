445615b90a0252ef593c9cdab979af0024d7949d
Aleksey Denisov <deoz@yandex.ru>
2021-07-29 01:46:15 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Collapse from './components/Collapse';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Collapse
+      collapsedLabel='Подробнее'
+      expandedLabel='Скрыть'
+      isExpanded={false}
+    >
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius libero quis odio aliquet, vel posuere odio pulvinar. Sed urna nibh, pretium ut sodales in, vestibulum eu eros. Duis sollicitudin sagittis felis, eget lobortis tellus venenatis iaculis. Mauris vel metus tortor. Nulla in lacus sodales, maximus purus aliquam, lobortis ex. Donec tempus massa at cursus condimentum. Duis suscipit ac nunc sit amet pretium. In ante leo, sollicitudin at volutpat id, gravida non felis. Morbi vehicula arcu a lorem convallis auctor. Aenean nibh mi, aliquam fermentum ante ac, mattis mattis lorem. Quisque rhoncus magna eget vehicula gravida.
+      </p>
+    </Collapse>
   );
 }
 
